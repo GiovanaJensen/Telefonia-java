5a55469e7fbf743e903e4ede5fd46a5d8b7f0229
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-11-03 20:00:40 -0300

    cadastrar assinante funcionando!

--- src/App.java
+++ src/App.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner entrada = new Scanner(System.in);
+        int opcao = 0;
+        Telefonia telefonia = new Telefonia();
+
+        while (opcao != 8) {
+            System.out.println("----MENU----");
+            System.out.println("1- Cadastrar Assinante");
+            System.out.println("2- Listar Assinantes");
+            System.out.println("3- Fazer Chamada");
+            System.out.println("4- Fazer Recarga");
+            System.out.println("5- Imprimir Fatura");
+            System.out.println("6- Localizar Pré-Pago");
+            System.out.println("7- Localizar Pós-Pago");
+            System.out.println("8- Sair do Programa");
+
+            opcao = entrada.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    telefonia.cadastrarAssinante();
+                    break;
+                case 2:
+                    // telefonia.listarAssinantes();
+                    break;
+                // other cases
+                default:
+                    System.out.println("Insira uma opção válida!");
+                    break;
+            }
+        }
+
+        entrada.close();
     }
 }
--- src/Telefonia.java
+++ src/Telefonia.java
@@ -1,42 +1,62 @@
+import java.util.Scanner;
+
 public class Telefonia {
-    private int numPrePagos;
-    private int numPosPagos;
-    private PrePago prePagos[];
-    private PosPago posPago[];
+    private int numPrePagos = 0;
+    private int numPosPagos = 0;
+    private PrePago prePagos[] = new PrePago[5];
+    private PosPago posPagos[] = new PosPago[5];
 
-    public Telefonia(){
+    public Telefonia() {}
 
+    public void cadastrarAssinante() {
+        Scanner entrada = new Scanner(System.in);
+        int opcao = 0, numero;
+        String nome;
+        long cpf;
+        float assinatura;
+
+        while (opcao != 1 && opcao != 2) {
+            System.out.println("Qual o tipo de assinatura: \n1-Pré-Pago\n2-Pós-Pago");
+            opcao = entrada.nextInt();
+        }
+
+        System.out.println("Digite o nome do assinante: ");
+        nome = entrada.next();
+        System.out.println("Digite o cpf do assinante: ");
+        cpf = entrada.nextLong();
+        System.out.println("Digite o n° de telefone: ");
+        numero = entrada.nextInt();
+
+        switch(opcao) {
+            case 1:
+                PrePago novoPrePago = new PrePago(cpf, nome, numero);
+                if (numPrePagos < prePagos.length) {
+                    prePagos[numPrePagos++] = novoPrePago;
+                    System.out.println("Pré-Pago criado com sucesso!");
+                } else {
+                    System.out.println("Limite de Pré-Pagos atingido!");
+                }
+                break;
+
+            case 2:
+                System.out.println("Digite a assinatura: ");
+                assinatura = entrada.nextFloat();
+                PosPago novoPosPago = new PosPago(cpf, nome, numero, assinatura);
+                if (numPosPagos < posPagos.length) {
+                    posPagos[numPosPagos++] = novoPosPago;
+                    System.out.println("Pós-Pago criado com sucesso!");
+                } else {
+                    System.out.println("Limite de Pós-Pagos atingido!");
+                }
+                break;
+
+            default:
+                System.out.println("Insira uma opção válida!");
+                break;
+        }
     }
 
-    public void cadastrarAssinante(){
-
-    }
-
-    public void listarAssinantes(){
-
-    }
-
-    public void fazerChamada(){
-
-    }
-
-    public void fazerRecarga(){
-
-    }
-
-    // private PrePago localizarPrePago(long cpf){
-        
-    // }
-
-    // private PosPago localizarPosPago(long cpf){
-
-    // }
-
-    public void imprimirFatura(){
-
-    }
-
-    public void main(String[] args){
+    public static void main(String[] args) {
         
     }
 }
