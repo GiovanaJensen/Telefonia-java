d814dad1fcb6ca6b241cd8ca64591f93df3c6f3a
andreasalgadosp <andrea.computadores@hotmail.com>
2023-11-14 08:43:30 -0300

    implementacao do casos imprimir fatura elocalizar pos e pre pago

--- src/App.java
+++ src/App.java
@@ -26,6 +26,19 @@ public class App {
                 case 2:
                     telefonia.listarAssinantes();
                     break;
+                case 5: 
+                    System.out.println("Digite o mes que deseja ver as faturas: "); 
+                    int mes = entrada.nextInt(); 
+                    telefonia.imprimirFatura(mes); 
+                    break; 
+                case 6: 
+                    System.out.println("Digite seu cpf: "); 
+                    long cpf = entrada.nextLong(); 
+                    telefonia.localizarPrePago(cpf); 
+                    break; 
+                case 7: 
+                    telefonia.localizarPosPago(cpf); 
+                    break;
                 case 8:
                     System.exit(0);
                     break;
