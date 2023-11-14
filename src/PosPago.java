b9113293c61b0730014e7a2cee719bcfc9be79bf
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-11-14 16:27:11 -0300

    implementacao do metodo fazer chamada no pos pago

--- src/PosPago.java
+++ src/PosPago.java
@@ -9,7 +9,17 @@ public class PosPago extends Assinante{
     }
 
     public void fazerChamada(Date data, int duracao){
-        
+        float ValorPos = (float)(duracao * 1.05);
+
+        if (numChamadas < chamadas.length){
+            Chamada novaChamada = new Chamada(data,duracao);
+            chamadas[numChamadas] = novaChamada;
+            ValorPos += ValorPos;
+            numChamadas++;
+            System.out.println("Chamada Pos Paga realizada com sucesso!");
+        }else{
+            System.out.println("Você não tem os requisitos necessários para realizar a chamada Pos Paga");
+        } 
     }
 
     public void imprimirFatura(int mes){
